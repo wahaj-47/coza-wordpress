b3052f52d16c118e65e4997d66ce09bc0ed4daf0
Wahaj Hussain <wahajhussain_47@hotmail.com>
2018-12-21 23:31:09 +0500

    Footer styles

--- footer.php
+++ footer.php
@@ -1,41 +1,43 @@
         <?php wp_footer(); ?>
         <footer>
-            <div class="row">
-                <div class="footer-container col-sm-6 col-lg-3">
-                    <h4>Categories</h4>
-                    <ul>
-                        <li>Women</li>
-                        <li>Men</li>
-                        <li>Shoes</li>
-                        <li>Watches</li>
-                    </ul>
-                </div>
-                <div class="footer-container col-sm-6 col-lg-3">
-                    <h4>Help</h4>
-                    <ul>
-                        <li>Track Order</li>
-                        <li>Returns</li>
-                        <li>Shipping</li>
-                        <li>FAQs</li>
-                    </ul>
-                </div>
-                <div class="footer-container col-sm-6 col-lg-3">
-                    <h4>Get in touch</h4>
-                    <ul>
-                        <li>Women</li>
-                        <li>Men</li>
-                        <li>Shoes</li>
-                        <li>Watches</li>
-                    </ul>
-                </div>
-                <div class="footer-container col-sm-6 col-lg-3">
-                    <h4>Newsletter</h4>
-                    <ul>
-                        <li>Women</li>
-                        <li>Men</li>
-                        <li>Shoes</li>
-                        <li>Watches</li>
-                    </ul>
+            <div class="container">
+                <div class="row">
+                    <div class="footer-container col-sm-6 col-lg-3">
+                        <h4>Categories</h4>
+                        <ul>
+                            <li>Women</li>
+                            <li>Men</li>
+                            <li>Shoes</li>
+                            <li>Watches</li>
+                        </ul>
+                    </div>
+                    <div class="footer-container col-sm-6 col-lg-3">
+                        <h4>Help</h4>
+                        <ul>
+                            <li>Track Order</li>
+                            <li>Returns</li>
+                            <li>Shipping</li>
+                            <li>FAQs</li>
+                        </ul>
+                    </div>
+                    <div class="footer-container col-sm-6 col-lg-3">
+                        <h4>Get in touch</h4>
+                        <ul>
+                            <li>Women</li>
+                            <li>Men</li>
+                            <li>Shoes</li>
+                            <li>Watches</li>
+                        </ul>
+                    </div>
+                    <div class="footer-container col-sm-6 col-lg-3">
+                        <h4>Newsletter</h4>
+                        <ul>
+                            <li>Women</li>
+                            <li>Men</li>
+                            <li>Shoes</li>
+                            <li>Watches</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </footer>
